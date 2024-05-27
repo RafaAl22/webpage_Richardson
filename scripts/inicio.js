d5a17a6307328f01cb13ddd8860461b504657e5f
RafaAl22 <jraa1141998j@gmail.com>
2024-05-27 16:01:57 -0600

    Diseño del index

---
+++ scripts/inicio.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+        
+    },
+    /*autoplay:{
+        delay:5000
+    },*/
+    grabCursor: true,
+    loop: true
+});
