3982c6eebac5fb6831bbd76406fdb55c0adaef2d
RafaAl22 <jraa1141998j@gmail.com>
2024-05-29 16:57:30 -0600

    Modificación de página de inicio

--- scripts/inicio.js
+++ scripts/inicio.js
@@ -11,3 +11,11 @@ var swiper = new Swiper(".mySwiper", {
     loop: true,
     spaceBetween: 30,
 });
+
+/*[...document.querySelectorAll('.column')].map(column => {
+	column.style.setProperty('--animation', 'slide');
+	column.style.setProperty('height', '200%');
+	column.innerHTML = column.innerHTML + column.innerHTML;
+});
+*/
+
