cc0dd8a1a5f4b6add5cef29f14187e23255b0921
RafaAl22 <jraa1141998j@gmail.com>
2024-07-18 15:29:13 -0600

    Inserción de productos nuevos e implementación de background por marca en cada producto

--- scripts/producto_detalle.js
+++ scripts/producto_detalle.js
@@ -5,51 +5,316 @@ var btn1 = document.getElementById('btn1');
 var btn2 = document.getElementById('btn2');
 var flecha1 = document.getElementById('flecha1');
 var flecha2 = document.getElementById('flecha2');
+var background = document.getElementById('bg');
+var text = document.getElementById('texto');
+var text2 = document.getElementById('texto2');
+var truper = 0;
+var hermex = 0;
+var foset = 0;
+var fiero = 0;
+var pretul = 0;
+var volteck = 0;
+var klintek = 0;
+var yale = 0;
+var rugo = 0;
 
+if (background.classList.contains('bg_hermex')) {
+    hermex = 1;
+}
+if (background.classList.contains('bg_truper')) {
+    truper = 1;
+}
+if (background.classList.contains('bg_fiero')) {
+    fiero = 1;
+}
+if (background.classList.contains('bg_foset')) {
+    foset = 1;
+}
+if (background.classList.contains('bg_klintek')) {
+    klintek = 1;
+}
+if (background.classList.contains('bg_pretul')) {
+    pretul = 1;
+}
+if (background.classList.contains('bg_volteck')) {
+    volteck = 1;
+}
+if (background.classList.contains('bg_yale')) {
+    yale = 1;
+}
+if (background.classList.contains('bg_rugo')) {
+    rugo = 1;
+}
 
-document.getElementById('btn1').addEventListener("click", function(){
-    if (especificaciones.classList.contains("ocultar")){
-        especificaciones.classList.remove('ocultar');
-        btn1.classList.add('active');
-
-        if(!especificaciones2.classList.contains("ocultar")){
-            especificaciones2.classList.add('ocultar');
-            btn2.classList.remove('active');
-            flecha2.classList.remove('rotacion');
-        }
-
-    }else{
-        especificaciones.classList.add('ocultar');
-        btn1.classList.remove('active');
-    }
-
-    if(flecha1.classList.contains('rotacion')){
+document.getElementById('btn1').addEventListener("click", function () {
+    if (flecha1.classList.contains('rotacion')) {
         flecha1.classList.remove('rotacion');
-    }else{
+    } else {
         flecha1.classList.add('rotacion')
     }
 
+    if (especificaciones.classList.contains("ocultar")) {
+        especificaciones.classList.remove('ocultar');
+
+
+        if (truper == 1) {
+            btn1.classList.add('truper');
+            text.style.color = "white";
+            flecha1.style.color = "white";
+        } if (foset == 1) {
+            btn1.classList.add('foset');
+            text.style.color = "white";
+            flecha1.style.color = "white";
+        } if (hermex == 1) {
+            btn1.classList.add('hermex');
+            text.style.color = "white";
+            flecha1.style.color = "white";
+        } if (fiero == 1) {
+            btn1.classList.add('fiero');
+            text.style.color = "white";
+            flecha1.style.color = "white";
+        } if (pretul == 1) {
+            btn1.classList.add('pretul');
+            text.style.color = "white";
+            flecha1.style.color = "white";
+        } if (klintek == 1) {
+            btn1.classList.add('klintek');
+            text.style.color = "white";
+            flecha1.style.color = "white";
+        } if (volteck == 1) {
+            btn1.classList.add('volteck');
+            text.style.color = "white";
+            flecha1.style.color = "white";
+        } if (yale == 1) {
+            btn1.classList.add('yale');
+            text.style.color = "black";
+            flecha1.style.color = "black";
+        } if (rugo == 1) {
+            btn1.classList.add('rugo');
+            text.style.color = "white";
+            flecha1.style.color = "white";
+        }
+
+
+        if (!especificaciones2.classList.contains("ocultar")) {
+            especificaciones2.classList.add('ocultar');
+
+            flecha2.classList.remove('rotacion');
+
+            if (truper == 1) {
+                btn2.classList.remove('truper');
+                text2.style.color = "black";
+                flecha2.style.color = "black";
+            } if (foset == 1) {
+                btn2.classList.remove('foset');
+                text2.style.color = "black";
+                flecha2.style.color = "black";
+            } if (hermex == 1) {
+                btn2.classList.remove('hermex');
+                text2.style.color = "black";
+                flecha2.style.color = "black";
+            } if (fiero == 1) {
+                btn2.classList.remove('fiero');
+                text2.style.color = "black";
+                flecha2.style.color = "black";
+            } if (pretul == 1) {
+                btn2.classList.remove('pretul');
+                text2.style.color = "black";
+                flecha2.style.color = "black";
+            } if (klintek == 1) {
+                btn2.classList.remove('klintek');
+                text2.style.color = "black";
+                flecha2.style.color = "black";
+            } if (volteck == 1) {
+                btn2.classList.remove('volteck');
+                text2.style.color = "black";
+                flecha2.style.color = "black";
+            } if (yale == 1) {
+                btn2.classList.remove('yale');
+                text2.style.color = "black";
+                flecha2.style.color = "black";
+            } if (rugo == 1) {
+                btn2.classList.remove('rugo');
+                text2.style.color = "black";
+                flecha2.style.color = "black";
+            }
+        }
+
+    } else {
+        especificaciones.classList.add('ocultar');
+
+        if (truper == 1) {
+            btn1.classList.remove('truper');
+            text.style.color = "black";
+            flecha1.style.color = "black";
+        } if (foset == 1) {
+            btn1.classList.remove('foset');
+            text.style.color = "black";
+            flecha1.style.color = "black";
+        } if (hermex == 1) {
+            btn1.classList.remove('hermex');
+            text.style.color = "black";
+            flecha1.style.color = "black";
+        } if (fiero == 1) {
+            btn1.classList.remove('fiero');
+            text.style.color = "black";
+            flecha1.style.color = "black";
+        } if (pretul == 1) {
+            btn1.classList.remove('pretul');
+            text.style.color = "black";
+            flecha1.style.color = "black";
+        } if (klintek == 1) {
+            btn1.classList.remove('klintek');
+            text.style.color = "black";
+            flecha1.style.color = "black";
+        } if (volteck == 1) {
+            btn1.classList.remove('volteck');
+            text.style.color = "black";
+            flecha1.style.color = "black";
+        } if (yale == 1) {
+            btn1.classList.remove('yale');
+            text.style.color = "black";
+            flecha1.style.color = "black";
+        } if (rugo == 1) {
+            btn1.classList.remove('rugo');
+            text.style.color = "black";
+            flecha1.style.color = "black";
+        }
+    }
 });
 
-document.getElementById('btn2').addEventListener("click", function(){
-    if (especificaciones2.classList.contains("ocultar")){
-        especificaciones2.classList.remove('ocultar');
-        btn2.classList.add('active');
+document.getElementById('btn2').addEventListener("click", function () {
 
-        if(!especificaciones.classList.contains("ocultar")){
-            especificaciones.classList.add('ocultar');
-            btn1.classList.remove('active');
-            flecha1.classList.remove('rotacion');
-        }
-    }else{
-        especificaciones2.classList.add('ocultar');
-        btn2.classList.remove('active');
-    }
-
-    if(flecha2.classList.contains('rotacion')){
+    if (flecha2.classList.contains('rotacion')) {
         flecha2.classList.remove('rotacion');
-    }else{
+    } else {
         flecha2.classList.add('rotacion')
     }
 
+    if (especificaciones2.classList.contains("ocultar")) {
+        especificaciones2.classList.remove('ocultar');
+
+        if (truper == 1) {
+            btn2.classList.add('truper');
+            text2.style.color = "white";
+            flecha2.style.color = "white";
+        } if (foset == 1) {
+            btn2.classList.add('foset');
+            text2.style.color = "white";
+            flecha2.style.color = "white";
+        } if (hermex == 1) {
+            btn2.classList.add('hermex');
+            text2.style.color = "white";
+            flecha2.style.color = "white";
+        } if (fiero == 1) {
+            btn2.classList.add('fiero');
+            text2.style.color = "white";
+            flecha2.style.color = "white";
+        } if (pretul == 1) {
+            btn2.classList.add('pretul');
+            text2.style.color = "white";
+            flecha2.style.color = "white";
+        } if (klintek == 1) {
+            btn2.classList.add('klintek');
+            text2.style.color = "white";
+            flecha2.style.color = "white";
+        } if (volteck == 1) {
+            btn2.classList.add('volteck');
+            text2.style.color = "white";
+            flecha2.style.color = "white";
+        } if (yale == 1) {
+            btn2.classList.add('yale');
+            text2.style.color = "black";
+            flecha2.style.color = "black";
+        } if (rugo == 1) {
+            btn2.classList.add('rugo');
+            text2.style.color = "white";
+            flecha2.style.color = "white";
+        }
+
+
+        if (!especificaciones.classList.contains("ocultar")) {
+            especificaciones.classList.add('ocultar');
+
+            flecha1.classList.remove('rotacion');
+
+            if (truper == 1) {
+                btn1.classList.remove('truper');
+                text.style.color = "black";
+                flecha1.style.color = "black";
+            } if (foset == 1) {
+                btn1.classList.remove('foset');
+                text.style.color = "black";
+                flecha1.style.color = "black";
+            } if (hermex == 1) {
+                btn1.classList.remove('hermex');
+                text.style.color = "black";
+                flecha1.style.color = "black";
+            } if (fiero == 1) {
+                btn1.classList.remove('fiero');
+                text.style.color = "black";
+                flecha1.style.color = "black";
+            } if (pretul == 1) {
+                btn1.classList.remove('pretul');
+                text.style.color = "black";
+                flecha1.style.color = "black";
+            } if (klintek == 1) {
+                btn1.classList.remove('klintek');
+                text.style.color = "black";
+                flecha1.style.color = "black";
+            } if (volteck == 1) {
+                btn1.classList.remove('volteck');
+                text.style.color = "black";
+                flecha1.style.color = "black";
+            } if (yale == 1) {
+                btn1.classList.remove('yale');
+                text.style.color = "black";
+                flecha1.style.color = "black";
+            } if (rugo == 1) {
+                btn1.classList.remove('rugo');
+                text.style.color = "black";
+                flecha1.style.color = "black";
+            }
+        }
+    } else {
+        especificaciones2.classList.add('ocultar');
+
+        if (truper == 1) {
+            btn2.classList.remove('truper');
+            text2.style.color = "black";
+            flecha2.style.color = "black";
+        } if (foset == 1) {
+            btn2.classList.remove('foset');
+            text2.style.color = "black";
+            flecha2.style.color = "black";
+        } if (hermex == 1) {
+            btn2.classList.remove('hermex');
+            text2.style.color = "black";
+            flecha2.style.color = "black";
+        } if (fiero == 1) {
+            btn2.classList.remove('fiero');
+            text2.style.color = "black";
+            flecha2.style.color = "black";
+        } if (pretul == 1) {
+            btn2.classList.remove('pretul');
+            text2.style.color = "black";
+            flecha2.style.color = "black";
+        } if (klintek == 1) {
+            btn2.classList.remove('klintek');
+            text2.style.color = "black";
+            flecha2.style.color = "black";
+        } if (volteck == 1) {
+            btn2.classList.remove('volteck');
+            text2.style.color = "black";
+            flecha2.style.color = "black";
+        } if (yale == 1) {
+            btn2.classList.remove('yale');
+            text2.style.color = "black";
+            flecha2.style.color = "black";
+        } if (rugo == 1) {
+            btn2.classList.remove('rugo');
+            text2.style.color = "black";
+            flecha2.style.color = "black";
+        }
+    }
 });
