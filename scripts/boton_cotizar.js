0545e5c75ef18a4b0a4fcb8f1d48ab7078d456a4
sabalprac <sabalprac@gmail.com>
2024-09-19 09:00:59 -0600

    letra cambio

--- scripts/boton_cotizar.js
+++ scripts/boton_cotizar.js
@@ -1,34 +1,5 @@
 /*Botón para cotizar a través de Whatsapp*/
 
-/*var seccion = document.getElementById("seccion");
-var info = document.getElementById("container_info");
-var bg = document.getElementById("bg");
-
-
-
-
-window.addEventListener("click",function(event) {
-    if (event.target == info || event.target == seccion || event.target == bg) {
-        btn_sur.classList.add('ocultar');
-        btn_norte.classList.add('ocultar');
-        btn_leon.classList.add('ocultar');
-        btn_cerrar.classList.add('ocultar');
-        btn_whatsapp.classList.remove('ocultar');
-      }
-  });
-
-  function cerrar_menubotton(){
-    if (btn_whatsapp.classList.contains("!ocultar")) {
-        btn_sur.classList.add('ocultar');
-        btn_norte.classList.add('ocultar');
-        btn_leon.classList.add('ocultar');
-        btn_cerrar.classList.add('ocultar');
-    };
-    
-  }
- */
-  
-
 var codigo = document.getElementById('codigo');
 var encabezado = document.getElementById('encabezado');
 var btn_sur = document.getElementById('abrir_sur');
@@ -36,10 +7,6 @@ var btn_norte = document.getElementById('abrir_norte');
 var btn_leon = document.getElementById('abrir_leon');
 var btn_cerrar = document.getElementById('cerrar_whatsapp');
 var btn_whatsapp = document.getElementById('abrir_whatsapp');
-
-
-
-
 
 btn_cerrar.addEventListener('click', function () {
     btn_sur.classList.add('ocultar_btn');
@@ -91,5 +58,3 @@ var url = "https://api.whatsapp.com/send?text=" + encodeURIComponent(mensaje) + 
 
 window.open(url);
 });
-
-
