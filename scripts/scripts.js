b1b31b77195b5fada0f442a74aec057db8f7900b
RafaAl22 <jraa1141998j@gmail.com>
2024-10-08 16:30:25 -0600

    Implementación de efectos de transición en productos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -31,37 +31,37 @@ window.onscroll = () => {
             };
         });*/
 
-/*sticky navbar */
+    /*sticky navbar */
 
-let header = document.querySelector('.header');
-header.classList.toggle('sticky', window.scrollY > 50);
+    let header = document.querySelector('.header');
+    header.classList.toggle('sticky', window.scrollY > 50);
 
-/*remove menu icon when click navbar link*/
+    /*remove menu icon when click navbar link*/
 
-menuIcon.classList.remove('bx-x');
-navbar.classList.remove('active');
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 
 };
 
 
 //Transiciones con scroll reveal
 
-ScrollReveal({ 
+ScrollReveal({
     reset: true,
     distance: '80px',
 });
 
-ScrollReveal().reveal('.cat-box', {scale: .95, duration: 500});
-ScrollReveal().reveal('.scroll_card', {interval: 45, duration: 500});
-ScrollReveal().reveal('.pregunta-box, .valores-box', {scale: .95, duration:1100});
-ScrollReveal().reveal('.tags, .nosotros h1', {origin: 'left', duration:1000});
-ScrollReveal().reveal('.marcas, .cards-box, .logo-img', {scale: .45, duration: 1000,});
-ScrollReveal().reveal('.texto-container, .info-container span', { origin: 'bottom', duration: 2100,});
-ScrollReveal().reveal('.cinta-lema, .text-rugo h1, .footer img, .texts h1', { origin: 'left', duration: 2500,});
-ScrollReveal().reveal('.img_banner, .info-container h2', { origin: 'right', duration: 2000,});
-ScrollReveal().reveal('.search-box', { origin: 'top', duration: 3500,});
-ScrollReveal().reveal('.section-cat h1, .categoria-section h1, .text-container_banner', { origin: 'top', duration: 500,});
-ScrollReveal().reveal('.item', {origin: 'right', duration:2000});
+ScrollReveal().reveal('.cat-box, .cat-box h2, .producto', { scale: .95, duration: 500 });
+ScrollReveal().reveal('.scroll_card', { interval: 45, duration: 500 });
+ScrollReveal().reveal('.valores-box', { scale: .95, duration: 1100 });
+ScrollReveal().reveal('.tags, .nosotros h1', { origin: 'left', duration: 1000 });
+ScrollReveal().reveal('.marcas, .cards-box, .logo-img', { scale: .45, duration: 1000, });
+ScrollReveal().reveal('.texto-container, .info-container span', { origin: 'bottom', duration: 2100, });
+ScrollReveal().reveal('.cinta-lema, .text-rugo h1, .footer img, .texts h1', { origin: 'left', duration: 2500, });
+ScrollReveal().reveal('.img_banner, .info-container h2', { origin: 'right', duration: 2000, });
+ScrollReveal().reveal('.search-box', { origin: 'top', duration: 3500, });
+ScrollReveal().reveal('.section-cat h1, .categoria-section h1, .text-container_banner, .section-top_products h1', { origin: 'top', duration: 500, });
+ScrollReveal().reveal('.item', { origin: 'right', duration: 2000 });
 
 
 
