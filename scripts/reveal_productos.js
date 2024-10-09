9a574b1b2d38f198b80d061782c3830ae6536807
sabalprac <sabalprac@gmail.com>
2024-10-09 14:37:04 -0600

    cotizar info

--- scripts/reveal_productos.js
+++ scripts/reveal_productos.js
@@ -9,4 +9,4 @@ ScrollReveal().reveal('.marca-container img', { origin: "top", duration: 3000 })
 ScrollReveal().reveal('.info-container', {origin:"bottom", duration:3000} ) 
 ScrollReveal().reveal('.marca-container h2, .specs-container', { origin: "left", duration: 800 });
 ScrollReveal().reveal('.img-swiper', {scale: .95, duration: 1000});
-ScrollReveal().reveal('.btn_whatsapp', {origin:"left", duration: 3500})
+//ScrollReveal().reveal('.btn_whatsapp', {origin:"left", duration: 3500})
